70112ce234924d0c0e92c30d779fb0feebc681b9
Tomas <tomas.huerta.94@gmail.com>
2020-02-04 22:20:12 -0800

    Initial Commit

---
+++ Chico Admin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chico Admin
+//
+//  Created by Tomas Galvan-Huerta on 2/4/20.
+//  Copyright © 2020 Somat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
